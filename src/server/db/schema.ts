630e2c31508a90d19741e963ee96d264c6cc1bc0
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-08-15 16:11:50 +0700

    feat(auth): add database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,16 +1,84 @@
-import { sql } from "drizzle-orm";
-import { index, pgTable } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
 
-export const posts = pgTable(
-  "posts",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+export const user = pgTable("users", (d) => ({
+  id: d.text().primaryKey(),
+  name: d.text().notNull(),
+  email: d.text().notNull().unique(),
+  emailVerified: d
+    .boolean("email_verified")
+    .$defaultFn(() => false)
+    .notNull(),
+  image: d.text(),
+  createdAt: d
+    .timestamp("created_at", { mode: "date", withTimezone: true })
+    .$defaultFn(() => new Date())
+    .notNull(),
+  updatedAt: d
+    .timestamp("updated_at", { mode: "date", withTimezone: true })
+    .$defaultFn(() => new Date())
+    .notNull(),
+}));
+
+export const session = pgTable("sessions", (d) => ({
+  id: d.text().primaryKey(),
+  expiresAt: d
+    .timestamp("expires_at", { mode: "date", withTimezone: true })
+    .notNull(),
+  token: d.text().notNull().unique(),
+  createdAt: d
+    .timestamp("created_at", { mode: "date", withTimezone: true })
+    .notNull(),
+  updatedAt: d
+    .timestamp("updated_at", { mode: "date", withTimezone: true })
+    .notNull(),
+  ipAddress: d.text("ip_address"),
+  userAgent: d.text("user_agent"),
+  userId: d
+    .text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+}));
+
+export const account = pgTable("accounts", (d) => ({
+  id: d.text().primaryKey(),
+  accountId: d.text("account_id").notNull(),
+  providerId: d.text("provider_id").notNull(),
+  userId: d
+    .text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  accessToken: d.text("access_token"),
+  refreshToken: d.text("refresh_token"),
+  idToken: d.text("id_token"),
+  accessTokenExpiresAt: d.timestamp("access_token_expires_at", {
+    mode: "date",
+    withTimezone: true,
   }),
-  (t) => [index("name_idx").on(t.name)],
-);
+  refreshTokenExpiresAt: d.timestamp("refresh_token_expires_at", {
+    mode: "date",
+    withTimezone: true,
+  }),
+  scope: d.text(),
+  password: d.text(),
+  createdAt: d
+    .timestamp("created_at", { mode: "date", withTimezone: true })
+    .notNull(),
+  updatedAt: d
+    .timestamp("updated_at", { mode: "date", withTimezone: true })
+    .notNull(),
+}));
+
+export const verification = pgTable("verifications", (d) => ({
+  id: d.text().primaryKey(),
+  identifier: d.text().notNull(),
+  value: d.text().notNull(),
+  expiresAt: d
+    .timestamp("expires_at", { mode: "date", withTimezone: true })
+    .notNull(),
+  createdAt: d
+    .timestamp("created_at", { mode: "date", withTimezone: true })
+    .$defaultFn(() => new Date()),
+  updatedAt: d
+    .timestamp("updated_at", { mode: "date", withTimezone: true })
+    .$defaultFn(() => new Date()),
+}));
