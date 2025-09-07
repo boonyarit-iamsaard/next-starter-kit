04ff68f18d42675db134a58eb4ac79f01aa07c45
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-09-07 19:13:32 +0700

    feat: display avatar in user info

--- src/common/components/user-info.tsx
+++ src/common/components/user-info.tsx
@@ -1,3 +1,9 @@
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/common/components/ui/avatar";
+import { getInitials } from "~/common/helpers/string";
 import type { Session } from "~/core/auth";
 
 interface UserInfoProps {
@@ -6,11 +12,19 @@ interface UserInfoProps {
 
 export function UserInfo({ user }: UserInfoProps) {
   return (
-    <div className="grid flex-1 text-left text-sm leading-tight">
-      <span className="truncate font-medium">{user.name}</span>
-      <span className="text-muted-foreground truncate text-xs">
-        {user.email}
-      </span>
-    </div>
+    <>
+      <Avatar className="h-8 w-8 overflow-hidden rounded-full">
+        {user.image ? <AvatarImage src={user.image} alt={user.name} /> : null}
+        <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+          {getInitials(user.name)}
+        </AvatarFallback>
+      </Avatar>
+      <div className="grid flex-1 text-left text-sm leading-tight">
+        <span className="truncate font-medium">{user.name}</span>
+        <span className="text-muted-foreground truncate text-xs">
+          {user.email}
+        </span>
+      </div>
+    </>
   );
 }
