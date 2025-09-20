21c577f31fd4e8e58f27edbacf800467ef40f71d
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-09-20 20:50:46 +0700

    fix: missing suspense boundary

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,10 +1,15 @@
+import { Suspense } from "react";
+
+import { LoadingSpinner } from "~/common/components/loading-spinner";
 import { UserTable } from "~/features/users/components/user-table";
 
 export default function AdminUserListPage() {
   return (
     <div className="space-y-4 p-4">
       <div className="text-2xl font-bold">Users</div>
-      <UserTable />
+      <Suspense fallback={<LoadingSpinner />}>
+        <UserTable />
+      </Suspense>
     </div>
   );
 }
